18683fcfa50ca44fa9b76fdc84910736b6492ac2
gofabian <me@gofabian.de>
2024-11-23 21:15:53 +0100

    Refactor download

--- screener/__init__.py
+++ screener/__init__.py
@@ -1,9 +1,11 @@
 from screener.download import download_195m
 from screener.finance import generate_analysis
 from screener.htmlgen import update_website
+from datetime import timedelta
 
 
 def generate(stocks, end):
-    dfs_195m = download_195m(stocks, end)
+    start = end - timedelta(days=200)
+    dfs_195m = download_195m(stocks, start, end)
     dfs = [generate_analysis(df_195m) for df_195m in dfs_195m]
     update_website(dfs)
