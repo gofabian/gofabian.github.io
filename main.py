8474f452b97e690e4711f876f41fc24116dfceb6
Fabian Iffländer <fabian.ifflaender@edag.com>
2024-11-25 22:07:02 +0100

    Update old reports with new stocks list

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from pytz import timezone
+
 from screener import generate
 from stocks import stocks
 
@@ -11,5 +13,5 @@ from stocks import stocks
 # end = datetime(year=2024, month=11, day=14, hour=12, minute=45, second=0, microsecond=0)
 # end = datetime(year=2024, month=11, day=12, hour=16, minute=00, second=0, microsecond=0)
 
-end = datetime(year=2024, month=11, day=23, hour=16, minute=0, second=0, microsecond=0)
+end = datetime.now(tz=timezone("America/New_York")).replace(tzinfo=None)
 generate(stocks, end)
