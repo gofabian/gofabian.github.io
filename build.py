a988567617ee3f2b9b5ea287ba09543817491fd3
gofabian <me@gofabian.de>
2024-11-26 21:32:57 +0100

    Refactor build times

--- build.py
+++ build.py
@@ -1,27 +1,44 @@
 import os
-import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
+from time import sleep
 
 from pytz import timezone
 
 from screener import generate
 from stocks import stocks
 
-previous_start = datetime.fromisoformat(
-    sorted([f for f in os.listdir("docs") if os.path.isdir(f"docs/{f}")])[-1]
-)
+end_times = [
+    # time(hour=12, minute=0),  # PJM
+    time(hour=12, minute=45),  # 1st 195m candle
+    # time(hour=15, minute=0),  # PJM
+    time(hour=16, minute=0)  # 2nd 195m candle
+]
+
 tz_new_york = timezone('America/New_York')
 tz_berlin = timezone('Europe/Berlin')
-previous_start = tz_berlin.localize(previous_start).astimezone(tz_new_york)
 
-if previous_start.hour >= 12:
-    next_start = previous_start.replace(hour=9, minute=30) + timedelta(days=1)
-    if next_start.weekday() > 4:
-        next_start += timedelta(days=7 - next_start.weekday())
-else:
-    next_start = previous_start.replace(hour=12, minute=45)
 
-next_end = next_start.replace(tzinfo=None) + timedelta(minutes=195)
+def get_previous_end():
+    previous_start = datetime.fromisoformat(
+        sorted([f for f in os.listdir("docs") if os.path.isdir(f"docs/{f}")])[-1]
+    )
+    previous_start = tz_berlin.localize(previous_start).astimezone(tz_new_york).replace(tzinfo=None)
+    return previous_start + timedelta(minutes=195)
+
+
+previous_end = get_previous_end()
+
+end_times = sorted(end_times, reverse=True)
+next_end = None
+for i, end_time in enumerate(end_times):
+    if previous_end.time() >= end_time:
+        next_end = datetime.combine(previous_end.date(), end_times[i - 1])
+        if next_end.time() < previous_end.time():
+            next_end += timedelta(days=1)
+        if next_end.weekday() > 4:
+            next_end += timedelta(days=7 - next_end.weekday())
+        break
+
 now = datetime.now(tz=tz_new_york).replace(tzinfo=None)
 
 print(f'Next report time: {next_end}')
@@ -32,9 +49,9 @@ if now >= next_end:
 elif now + timedelta(minutes=15) > next_end:
     while True:
         now = datetime.now(tz=tz_new_york).replace(tzinfo=None)
-        print(f'Waiting now={now.time()} until={next_end.time()}...')
+        print(f'Waiting now={now} until={next_end.time()}...')
         if now >= next_end:
             break
-        time.sleep(60)
+        sleep(60)
     print(f'Report due now -> generate')
     generate(stocks, next_end)
