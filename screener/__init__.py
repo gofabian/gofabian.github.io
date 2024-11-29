8e2ba4386152fa5f48bc0bbdd7eb8ac4972beac1
Fabian Iffländer <fabian.ifflaender@edag.com>
2024-11-29 09:16:52 +0100

    Let CI succeed on US holidays

--- screener/__init__.py
+++ screener/__init__.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
 
+from yfinance.exceptions import YFPricesMissingError
+
 from screener.cache import download_195m
 from screener.finance import generate_analysis
 from screener.htmlgen import update_website
@@ -13,8 +15,14 @@ def generate(stocks, end):
     print(f'Start: {start}')
     print(f'End: {end}\n')
 
-    progress_download = Progress('Download', stocks)
+    progress_download = Progress('Download', stocks, error_to_catch=YFPricesMissingError)
     dfs_195m = [progress_download.next(lambda: download_195m(stock, start, end)) for stock in stocks]
+    if len(progress_download.errors) == len(stocks):
+        # Probably US holiday
+        print('No data found for any stock -> Probably US holiday')
+        exit(0)
+    if progress_download.errors:
+        raise progress_download.errors[0]
 
     progress_analysis = Progress('Analysis', stocks)
     dfs = [progress_analysis.next(lambda: generate_analysis(df_195m)) for df_195m in dfs_195m]
