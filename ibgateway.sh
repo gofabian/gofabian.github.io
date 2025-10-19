cf68090782f42c36be59ac52557b65da932b2166
Fabian Iffländer <fabian.ifflaender@posteo.de>
2025-10-19 21:51:12 +0200

    wip

--- ibgateway.sh
+++ ibgateway.sh
@@ -6,8 +6,8 @@ docker run \
   -e TWS_PASSWORD \
   -e READ_ONLY_API=no \
   -e TRADING_MODE=paper \
-  --health-cmd "nc -z 127.0.0.1 4004 || exit 1" \
+  --health-cmd "bash -c 'echo > /dev/tcp/127.0.0.1/4004' || exit 1" \
   --health-interval 5s \
   --health-timeout 2s \
-  --health-retries 20 \
+  --health-retries 10 \
   ghcr.io/gnzsnz/ib-gateway:stable
