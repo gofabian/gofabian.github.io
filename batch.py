d87e06468bcc2dbbba0e148cb83224b1f3210206
gofabian <fabian.ifflaender@posteo.de>
2025-10-29 21:32:17 +0100

    fix sleep secs

--- batch.py
+++ batch.py
@@ -21,7 +21,7 @@ def batch(symbols: list[str], batch_size: int, sleep_secs: int, fn):
         fn(symbols_batch, batch_num, num_batches)
 
         if batch_num < num_batches - 1:
-            sleep_secs_rest = (datetime_before + timedelta(seconds=sleep_secs) - datetime.now()).total_seconds()
+            sleep_secs_rest = (datetime_before + timedelta(seconds=sleep_secs) - datetime.now()).total_seconds() // 1
             log(f"⏳ Warte {sleep_secs_rest}s, um IB-Pacing einzuhalten...")
             if sleep_secs_rest > 0:
-                time.sleep(sleep_secs)
+                time.sleep(sleep_secs_rest)
