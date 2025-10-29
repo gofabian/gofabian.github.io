0bb99a13ce026b34d6ec4a8ad0e5f2e567099681
gofabian <me@gofabian.de>
2025-10-29 20:59:10 +0100

    batch duration max. 10 minutes

--- batch.py
+++ batch.py
@@ -1,21 +1,27 @@
 import math
 import time
+from datetime import datetime, timedelta
+
+from log import log
 
 
 def batch(symbols: list[str], batch_size: int, sleep_secs: int, fn):
     total = len(symbols)
     num_batches = math.ceil(total / batch_size)
 
-    print(f"Verarbeite {total} Symbole in {num_batches} Batches...")
+    log(f"Verarbeite {total} Symbole in {num_batches} Batches...")
 
     for batch_num in range(num_batches):
         start = batch_num * batch_size
         end = min(start + batch_size, total)
         symbols_batch = symbols[start:end]
 
-        print(f"Batch {batch_num + 1}/{num_batches}: {len(symbols_batch)} Symbols")
+        log(f"Batch {batch_num + 1}/{num_batches}: {len(symbols_batch)} Symbols")
+        datetime_before = datetime.now()
         fn(symbols_batch, batch_num, num_batches)
 
         if batch_num < num_batches - 1:
-            print(f"⏳ Warte 10 Minuten, um IB-Pacing einzuhalten...")
-            time.sleep(sleep_secs)
+            sleep_secs_rest = (datetime_before + timedelta(seconds=sleep_secs) - datetime.now()).total_seconds()
+            log(f"⏳ Warte {sleep_secs_rest}s, um IB-Pacing einzuhalten...")
+            if sleep_secs_rest > 0:
+                time.sleep(sleep_secs)
