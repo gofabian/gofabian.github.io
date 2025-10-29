a75a09e89a33472f39c6b2fe9f70db45031e25f8
gofabian <fabian.ifflaender@posteo.de>
2025-10-29 21:45:41 +0100

    flush printed messages

--- log.py
+++ log.py
@@ -3,4 +3,6 @@ from datetime import datetime
 
 def log(*args, **kwargs):
     timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+    kwargs = kwargs.copy()
+    kwargs['flush'] = True
     print(timestamp, *args, **kwargs)
