eac3b23b2bf61c4357d88645a69ee8dc16a30562
gofabian <me@gofabian.de>
2025-11-02 18:00:49 +0100

    Fix asyncio on Windows

--- source.py
+++ source.py
@@ -9,31 +9,27 @@ from log import log
 
 IB_HOST = '127.0.0.1'
 IB_PORT = 4002
+# IB_PORT = 7496
 FAST_MAX_CONCURRENT = 4
 SLOW_SECONDS_PER_REQUEST = 10
 
-
-# for long durations > '1 W' (depends on bar size)
-def download_symbol_slow(symbol: str, end: datetime, duration: str) -> DataFrame:
-    dt_start = datetime.now()
-    df = download_symbol(symbol, end, duration)
-    seconds_rest = SLOW_SECONDS_PER_REQUEST - (datetime.now() - dt_start).total_seconds()
-    log(f"Waiting {0 if seconds_rest < 0 else seconds_rest}s")
-    if seconds_rest > 0:
-        time.sleep(seconds_rest)
-    return df
+# fix asyncio even loop issues on Windows
+util.patchAsyncio()
 
 
 def download_symbol(symbol: str, end: datetime, duration: str) -> DataFrame:
-    ib = IB()
-    ib.connect(IB_HOST, IB_PORT, clientId=1)
-    df = _download_symbol_sync(ib, symbol, end, duration)
-    ib.disconnect()
-    return df
+    async def do_async() -> DataFrame:
+        ib = IB()
+        await ib.connectAsync(IB_HOST, IB_PORT, clientId=1)
+        df = await _download_symbol_async(ib, symbol, end, duration)
+        ib.disconnect()
+        return df
+
+    return asyncio.run(do_async())
 
 
 # for short durations < '1 W' (depends on bar size)
-def download_symbols_fast(symbols: list[str], end: datetime, duration: str) -> list[DataFrame]:
+def download_symbols_in_parallel(symbols: list[str], end: datetime, duration: str) -> list[DataFrame]:
     return asyncio.run(_download_symbols_async(symbols, end, duration))
 
 
@@ -67,25 +63,6 @@ async def _download_symbol_async(ib: IB, symbol: str, end: datetime, duration: s
     )
     log(f"Downloaded {symbol}")
 
-    return _convert_bars15_to_df195(symbol, end, bars)
-
-
-def _download_symbol_sync(ib: IB, symbol: str, end: datetime, duration: str) -> DataFrame:
-    log(f"Downloading symbol={symbol} end={end.isoformat()}, duration={duration}")
-    bars = ib.reqHistoricalData(
-        Stock(symbol, 'SMART', 'USD'),
-        endDateTime=end,
-        durationStr=duration,
-        barSizeSetting='15 mins',
-        whatToShow='TRADES',
-        useRTH=True
-    )
-    log(f"Downloaded {symbol}")
-
-    return _convert_bars15_to_df195(symbol, end, bars)
-
-
-def _convert_bars15_to_df195(symbol, end, bars) -> DataFrame:
     # switch to pandas world
     df_15 = util.df(bars)
 
